6950952da50e29cba43f39d981eebe15d1858e4b
Carl <carl.cerf@gmail.com>
2023-03-24 18:00:11 +0100

    dialog result component create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './game/board/board.component';
+import { DialogResultComponent } from './game/dialog-result/dialog-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DialogResultComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/game/dialog-result/dialog-result.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-result',
+  templateUrl: './dialog-result.component.html',
+  styleUrls: ['./dialog-result.component.scss']
+})
+export class DialogResultComponent {
+
+}
