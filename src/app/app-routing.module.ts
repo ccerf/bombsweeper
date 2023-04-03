bd3e385cccf8d25921bfaf5e58be2089fdc374cf
Carl <carl.cerf@gmail.com>
2023-04-03 23:55:19 +0200

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BoardComponent } from "./game/board/board.component";
+import { ScoresTableComponent } from "./game/scores-table/scores-table.component";
 
 const routes: Routes = [
 	{ path: "", component: BoardComponent },
-	// {path: 'high-scores', component: }
+	{ path: "high-scores", component: ScoresTableComponent },
 ];
 
 @NgModule({
